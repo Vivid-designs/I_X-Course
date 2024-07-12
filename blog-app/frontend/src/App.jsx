447a11de716d0179c3287e6058ede5a9a4cb71e9
Vivid-designs <lario@vividgraphics.co.za>
2024-07-12 13:30:43 +0200

    "Added react-router-dom and updated frontend code to include new components and routing"

--- blog-app/frontend/src/App.jsx
+++ blog-app/frontend/src/App.jsx
@@ -1,12 +1,11 @@
-// App.jsx
-import './App.css';
+import HomePage from "./components/HomePage";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <HomePage />
     </div>
   );
 }
-
 export default App;
